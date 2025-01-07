8b2f7d924fd3972b5b702f019fdfb8888681ae38
ReversityVII <justin.c.barry@gmail.com>
2025-01-07 17:09:26 -0500

    disabled re-imbursement and building effect
    As requested, these features have been disabled. They are commented out in case re-implementation later makes any sense.
    
    You can also destroy them if you so wish.

--- mayor-jubilee/Assets/Scripts/Pack/FigureBehaviour.cs
+++ mayor-jubilee/Assets/Scripts/Pack/FigureBehaviour.cs
@@ -38,8 +38,8 @@ public class FigureBehaviour : MonoBehaviour
         if(GameObject.Find(character.GetRecord(false).Name))
             {
             //if so, add money instead and then delete the object
-            moneyManagement = GameObject.FindObjectOfType<MoneyManagement>();
-            moneyManagement.AddMoney(packCost / 5); //reimburse 20 percent of what it cost
+            //moneyManagement = GameObject.FindObjectOfType<MoneyManagement>();
+            //moneyManagement.AddMoney(packCost / 5); //reimburse 20 percent of what it cost
 
             print("Already exists, destroying!");
             Destroy(this.gameObject);
@@ -47,12 +47,14 @@ public class FigureBehaviour : MonoBehaviour
 
         gameObject.name = character.GetRecord(false).Name;
 
+        //INFLUENCE BUILDING
         //reference the building that this figure influences
-        affectedBuildingSlot = GameObject.Find("Building Slot " + character.GetRecord(false).BuildingSlotAffected);
-        affectedBuilding = affectedBuildingSlot.GetComponentInChildren<BuildingBehaviour>();
+        //affectedBuildingSlot = GameObject.Find("Building Slot " + character.GetRecord(false).BuildingSlotAffected);
+        //affectedBuilding = affectedBuildingSlot.GetComponentInChildren<BuildingBehaviour>();
 
         //influence the building money production
-        affectedBuilding.buildingInfluence += character.GetRecord(false).Percentageearningboost;
+        //affectedBuilding.buildingInfluence += character.GetRecord(false).Percentageearningboost;
+
 
         //find and update text
         flavourText = (TextMeshProUGUI) gameObject.GetComponentInChildren(typeof(TextMeshProUGUI));
