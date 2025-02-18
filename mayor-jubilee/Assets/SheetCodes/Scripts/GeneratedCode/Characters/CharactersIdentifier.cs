0a772de968bc832f31390dfaf511f64f79794177
Nova-fudge <100056862+Nova-fudge@users.noreply.github.com>
2025-02-18 11:17:55 -0500

    playtest changes
    added fodder, adjusted character collection sprite, flavour text added

--- mayor-jubilee/Assets/SheetCodes/Scripts/GeneratedCode/Characters/CharactersIdentifier.cs
+++ mayor-jubilee/Assets/SheetCodes/Scripts/GeneratedCode/Characters/CharactersIdentifier.cs
@@ -10,6 +10,11 @@ namespace SheetCodes
 		[Identifier("test char 3")] TestChar3 = 3,
 		[Identifier("Character 5")] Character5 = 5,
 		[Identifier("test character 4")] TestCharacter4 = 4,
+		[Identifier("Fodder1")] Fodder1 = 6,
+		[Identifier("Fodder2")] Fodder2 = 7,
+		[Identifier("Fodder3")] Fodder3 = 8,
+		[Identifier("Fodder4")] Fodder4 = 9,
+		[Identifier("Fodder5")] Fodder5 = 10,
 	}
 
 	public static class CharactersIdentifierExtension
