3ef34308e13ed556bc7b2f116b8cecbbb262a54d
ReversityVII <127150723+ReversityVII@users.noreply.github.com>
2025-03-10 16:38:57 -0400

    basic endgame countdown
    can call stuff when this count ends.

---
+++ mayor-jubilee/Assets/Scripts/EndgameCountdown.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EndgameCountdown : MonoBehaviour
+{
+    public float totalTimeTilEnd;
+    private float timer = 0;
+    private float timeInIngameDays;
+
+    public Color[] textColors;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        //calculate time in ingame days
+        timeInIngameDays = totalTimeTilEnd - timer;
+        timeInIngameDays = timeInIngameDays / 60; //each day is a minute
+
+        //update text display
+        gameObject.GetComponent<TextMeshProUGUI>().text = Mathf.RoundToInt(timeInIngameDays) + " DAYS";
+
+        //update text color to match general urgency
+        if (timer > totalTimeTilEnd / 4) //<25% of time remaining
+        {
+            gameObject.GetComponent<TextMeshProUGUI>().color = textColors[2]; //RED
+        }
+        else if (timer > totalTimeTilEnd / 2) //<50% of time remaining
+        {
+            gameObject.GetComponent<TextMeshProUGUI>().color = textColors[1]; //ORANGE
+        }
+        else
+        {
+            gameObject.GetComponent<TextMeshProUGUI>().color = textColors[0]; //GREEN
+        }
+
+
+    }
+}
